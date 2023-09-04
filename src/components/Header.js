416ed679f7e4f9dffb17d74179c3933efdf4b6ae
tajemouti <tajemouti@gmail.com>
2023-09-04 15:42:24 +0100

    Add header component

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import planet from '../assets/planet.png';
+
+const Header = () => (
+  <header>
+    <div className="logo">
+      <img src={planet} alt="Logo" />
+      <h1>Space Travellers&apos; Hub</h1>
+    </div>
+    <ul>
+      <li />
+      <li />
+      <li />
+    </ul>
+  </header>
+);
+
+export default Header;
