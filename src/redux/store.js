0c2e81a3c7d6971e2cc46b4bc83653280e523771
Qaisar <kayser.abbas@gmail.com>
2023-09-05 18:19:33 +0500

    Update the code

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,6 +7,7 @@ const store = configureStore({
     missions: missionsReducer,
     rockets: rocketReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
 });
 
 export default store;
