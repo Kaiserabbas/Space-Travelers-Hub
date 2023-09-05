c38076a5a8ece6445e0e6a07097ba44b97d7870f
Qaisar <kayser.abbas@gmail.com>
2023-09-05 18:20:15 +0500

    Fetch data from the Missions endpoint

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchMissions } from '../redux/missions/missionsSlice';
 
 function Missions() {
-  return (
-    <div>
-      Missions
-    </div>
-  );
+  const dispatch = useDispatch();
+  const missions = useSelector((state) => state.missions.missions);
+  const status = useSelector((state) => state.missions.status);
+
+  useEffect(() => {
+    if (status === 'idle') {
+      dispatch(fetchMissions());
+    }
+  }, [status, dispatch]);
+
+  if (status === 'loading') {
+    return <div>Loading...</div>;
+  } if (status === 'succeeded') {
+    return (
+      <div>
+        {missions.map((mission) => (
+          <div key={mission.mission_id}>
+            <h2>{mission.mission_name}</h2>
+            <p>{mission.description}</p>
+          </div>
+        ))}
+      </div>
+    );
+  } if (status === 'failed') {
+    return <div>Error loading missions.</div>;
+  }
+
+  return null;
 }
 
 export default Missions;
