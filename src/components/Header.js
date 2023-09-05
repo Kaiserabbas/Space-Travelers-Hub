db742b2ec93ccc7b9aacb27bde938c19584f5e06
Qaisar <kayser.abbas@gmail.com>
2023-09-05 09:14:07 +0500

    Add <hr/> under the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,20 @@
 import planet from '../assets/planet.png';
 
 const Header = () => (
-  <header>
-    <div className="logo">
-      <img src={planet} alt="Logo" />
-      <h1>Space Travellers&apos; Hub</h1>
-    </div>
-    <ul>
-      <li />
-      <li />
-      <li />
-    </ul>
-  </header>
+  <>
+    <header>
+      <div className="logo">
+        <img src={planet} alt="Logo" />
+        <h1>Space Travelers&apos; Hub</h1>
+      </div>
+      <ul>
+        <li />
+        <li />
+        <li />
+      </ul>
+    </header>
+    <hr />
+  </>
 );
 
 export default Header;
