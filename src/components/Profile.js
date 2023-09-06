eea03ea936c841bf5d95e99a93cd7142489d5c8c
tajemouti <tajemouti@gmail.com>
2023-09-06 15:28:17 +0100

    Add some basic styling

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -20,20 +20,19 @@ function Profile() {
   };
   return (
     <div>
-      <h1>My Profile</h1>
       <div className="profile">
         <div className="rockets-profile">
           <h2>Rockets</h2>
-          <ul>
+          <ul className="rockets-list">
             {reservedRockets.map((rocket) => (
-              <li key={rocket.id}>
+              <li className="list-item" key={rocket.id}>
                 {rocket.rocket_name}
               </li>
             ))}
           </ul>
         </div>
         <div>
-          <h2>Joined Missions</h2>
+          <h2>Missions</h2>
           <ul className="missions-profile">
             {joinedMissions.map((mission) => (
               <li key={mission.mission_id} className="missions-list">
