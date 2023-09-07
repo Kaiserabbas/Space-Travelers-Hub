b66cc4686c1f22b00c8e5dc02af92614ac4596fd
tajemouti <tajemouti@gmail.com>
2023-09-07 13:32:06 +0100

    Test for clicking Reserve Rocket dispatches reserveRocket action

--- src/components/Rockets.test.js
+++ src/components/Rockets.test.js
@@ -1,10 +1,36 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 import Rockets from './Rockets';
+import { reserveRocket } from '../redux/rockets/rocketsSlice';
+
+const mockStore = configureMockStore([thunk]);
 
 describe('Rockets Component', () => {
+  let store;
+  beforeEach(() => {
+    store = mockStore({
+      rockets: {
+        rockets: [
+          {
+            id: '5e9d0d95eda69955f709d1eb',
+            rocket_name: 'Falcon 1',
+            reserved: false,
+          },
+          {
+            id: '5e9d0d95eda69973a809d1ec',
+            rocket_name: 'Falcon 9',
+            reserved: true,
+          },
+        ],
+        status: 'succeeded',
+      },
+    });
+  });
+
   test('Renders rockets and Reserve Rocket button', async () => {
     render(
       <Provider store={store}>
@@ -17,5 +43,18 @@ describe('Rockets Component', () => {
 
     expect(rocketNameElement).toBeInTheDocument();
     expect(reserveButtonElement).toBeInTheDocument();
+  });
+
+  test('Clicking Reserve Rocket dispatches reserveRocket action', async () => {
+    render(
+      <Provider store={store}>
+        <Rockets />
+      </Provider>,
+    );
+
+    const reserveButtonElement = screen.getByText('Reserve Rocket');
+    fireEvent.click(reserveButtonElement);
+
+    expect(store.getActions()).toContainEqual(reserveRocket({ id: '5e9d0d95eda69955f709d1eb' }));
   });
 });
